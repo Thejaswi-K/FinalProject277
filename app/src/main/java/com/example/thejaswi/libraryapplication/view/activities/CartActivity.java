bedd655f5bf39922aac7e313af5529dba583a240
Thejaswi-K <thejaswi.kampalli@sjsu.edu>
2017-12-07 15:09:42 -0800

    Checkout button added with a toast

--- app/src/main/java/com/example/thejaswi/libraryapplication/view/activities/CartActivity.java
+++ app/src/main/java/com/example/thejaswi/libraryapplication/view/activities/CartActivity.java
@@ -5,15 +5,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
-
+import android.widget.Toast;
 import com.example.thejaswi.libraryapplication.R;
 import com.example.thejaswi.libraryapplication.model.entities.Cart;
 import com.example.thejaswi.libraryapplication.view.fragment.CartBooksAdapter;
 
 public class CartActivity extends AppCompatActivity implements View.OnClickListener {
     RecyclerView clist;
-    Button go2cart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +20,12 @@ public class CartActivity extends AppCompatActivity implements View.OnClickListe
         clist.setLayoutManager(new LinearLayoutManager(this));
         clist.setAdapter(new CartBooksAdapter(this));
         findViewById(R.id.bk).setOnClickListener(this);
+        findViewById(R.id.go2check).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Checked out",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
