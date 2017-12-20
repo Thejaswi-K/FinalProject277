515955ed9bdeec5c33ec562e1481bf759ffd22b6
vivek <vivek.d.agarwal@sjsu.edu>
2017-12-20 15:26:37 -0800

    add to waitlist and reservation checks working.

---
+++ app/src/main/java/com/example/thejaswi/libraryapplication/view/activities/MyReservationActivity.java
@@ -0,0 +1,86 @@
+package com.example.thejaswi.libraryapplication.view.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.thejaswi.libraryapplication.R;
+import com.example.thejaswi.libraryapplication.domain.api.APIService;
+import com.example.thejaswi.libraryapplication.domain.api.ServiceGenerator;
+import com.example.thejaswi.libraryapplication.model.entities.BookIssuedInfo;
+import com.example.thejaswi.libraryapplication.model.entities.Catalog;
+import com.example.thejaswi.libraryapplication.view.fragment.MyReservationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by vivek on 12/20/2017.
+ */
+
+public class MyReservationActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    List<Catalog> catalogList=new ArrayList<>();
+    APIService mAPIService= ServiceGenerator.createService(APIService.class);
+    Call<List<Catalog>> reservedCatalogsList=mAPIService.getReservedCatalogs();
+    TextView topLabel;
+    MyReservationAdapter mra;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_order);
+        topLabel = (TextView) findViewById(R.id.top_label);
+        topLabel.setText("Reservations");
+        recyclerView=(RecyclerView) findViewById(R.id.orderlist);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mra=new MyReservationAdapter(this,catalogList);
+
+
+        reservedCatalogsList.enqueue(new Callback<List<Catalog>>() {
+            @Override
+            public void onResponse(Call<List<Catalog>> call, Response<List<Catalog>> response) {
+                catalogList=response.body();
+                Log.e("Reservation Activity","Catalog Reserved received as :"+catalogList);
+                if(response.code()==200) {
+                    if (catalogList.size() == 0) {
+                        Toast.makeText(getApplicationContext(), "No record found for books reserved", Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        recyclerView.setAdapter(mra);
+                        mra.setCatalogList(catalogList);
+                        mra.notifyDataSetChanged();
+                    }
+                }
+                if(response.code()==401){
+                    Toast.makeText(getApplicationContext(), "Please login again. Session absent.", Toast.LENGTH_LONG).show();
+                }
+                if(response.code()==500){
+                    Toast.makeText(getApplicationContext(), "Please try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Catalog>> call, Throwable t) {
+                Log.e("MyOrderActivity.java","Get issued books failed due to "+t.getCause());
+                Toast.makeText(getApplicationContext(),"Sorry. Could not make request. Please try again later.",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        findViewById(R.id.order_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/thejaswi/libraryapplication/view/fragment/MyWaitlistAdapter.java
@@ -0,0 +1,88 @@
+package com.example.thejaswi.libraryapplication.view.fragment;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.thejaswi.libraryapplication.R;
+import com.example.thejaswi.libraryapplication.model.entities.Catalog;
+import com.example.thejaswi.libraryapplication.view.activities.MyWaitlistActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by vivek on 12/20/2017.
+ */
+
+public class MyWaitlistAdapter extends RecyclerView.Adapter<MyWaitlistAdapter.Holder> {
+    Context context;
+    List<Catalog> catalogList=new ArrayList<>();
+
+    public MyWaitlistAdapter(MyWaitlistActivity myWaitlistActivity, List<Catalog> catalogList) {
+        this.catalogList=catalogList;
+        this.context=myWaitlistActivity;
+        Log.e("MyWaitlistAdapter","Inside the constructor for wait list adapter");
+    }
+
+    @Override
+    public MyWaitlistAdapter.Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.order_adapter, parent, false);
+        return new MyWaitlistAdapter.Holder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyWaitlistAdapter.Holder holder, int position) {
+        holder.bookTitle.setText("Title :"+catalogList.get(position).getTitle());
+        holder.bookAuthor.setText("Author :"+catalogList.get(position).getAuthor());
+        holder.bookStatus.setText("Status : RESERVED");
+        holder.bookPublisher.setText("Publisher :"+catalogList.get(position).getPublisher());
+        holder.bookYear.setText("Year :"+catalogList.get(position).getYear());
+        holder.addToCart.setText("Add To Cart");
+        holder.addToCart.setVisibility(View.GONE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return catalogList==null?0:catalogList.size();
+    }
+
+    public void setCatalogList(List<Catalog> catalogList) {
+        this.catalogList = catalogList;
+    }
+
+
+    public class Holder extends RecyclerView.ViewHolder {
+
+        TextView dueDate,bookedDate,bookTitle,bookAuthor,bookPublisher,bookYear,bookStatus,addToCart;
+        ImageView bookimage;
+        LinearLayout renewBook;
+        public Holder(View itemView) {
+            super(itemView);
+            bookedDate=(TextView)itemView.findViewById(R.id.order_bookdate);
+            bookedDate.setVisibility(View.GONE);
+            dueDate=(TextView)itemView.findViewById(R.id.order_duedate);
+            dueDate.setVisibility(View.GONE);
+            bookimage=(ImageView)itemView.findViewById(R.id.order_bookimage);
+            bookTitle = (TextView)itemView.findViewById(R.id.order_title);
+            bookAuthor=(TextView)itemView.findViewById(R.id.order_author);
+            bookPublisher=(TextView)itemView.findViewById(R.id.publisher);
+            bookYear=(TextView)itemView.findViewById(R.id.year);
+            bookStatus=(TextView)itemView.findViewById(R.id.status);
+            addToCart=(TextView)itemView.findViewById(R.id.single_action_text);
+
+        }
+    }
+}
